059a8c84688e9aea6d1bcd0144c5cfe913fa79d1
tomwot <tttomwottt@gmail.com>
2013-05-09 07:44:46 +0900

    最初のバージョンを登録。
    git-svn-id: file:///var/svn/repo/CodeIQ@13 a45ba2cc-39c1-924a-b1ab-54c9c331bf61

---
+++ Yuki/toggle/trunk/toggle.rb
@@ -0,0 +1,8 @@
+switch = (ARGV[0] || 10).to_i
+
+mask = 2**switch - 1
+answer = []
+
+(2**(switch-1)).times{|n| answer << (gray = n ^ (n >> 1)) << (gray ^ mask)}
+answer << answer.shift
+answer.map{|ans| printf "%0*b\n", switch, ans}
