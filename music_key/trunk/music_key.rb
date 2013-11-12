13700a71e39a7cc67d2163f94523a5ec43d19868
tomwot <tttomwottt@gmail.com>
2013-11-12 22:21:39 +0900

    sortしなくてもチェックできるように修正。 これこそ本当の初回投稿分。
    git-svn-id: file:///var/svn/repo/CodeIQ@61 a45ba2cc-39c1-924a-b1ab-54c9c331bf61

--- music_key/trunk/music_key.rb
+++ music_key/trunk/music_key.rb
@@ -3,8 +3,8 @@ major = sound_list.select{|s| s.length == 1}.map{|s| sound_list.index(s)}
 sound_lists = sound_list.map.with_index{|s, i| sound_list.values_at(*major.map{|m|(m+i)%12})}
 gets.to_i.times do
   n_sounds = gets.to_i
-  sounds = gets.chomp.split.uniq.sort
-  ans = sound_lists.select{|sl| sl.sort & sounds == sounds}
+  sounds = gets.chomp.split.uniq
+  ans = sound_lists.select{|sl| sounds & sl == sounds}
   if ans.empty?
     puts 'invalid'
   else
