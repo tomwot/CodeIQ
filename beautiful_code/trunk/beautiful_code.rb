2ef9fa5d9408c36faa9c80254028096c53ce535b
tomwot <tttomwottt@gmail.com>
2014-02-16 08:34:00 +0900

    Enumeratorを使って書き直し。
    git-svn-id: file:///var/svn/repo/CodeIQ@78 a45ba2cc-39c1-924a-b1ab-54c9c331bf61

--- beautiful_code/trunk/beautiful_code.rb
+++ beautiful_code/trunk/beautiful_code.rb
@@ -1,8 +1,9 @@
-n = 0
-b = []
-begin
-  m = (n+=1)
-  m /= 3 while m >= 3 && m % 3 != 2
-  b << n if m < 2
-end until b.size == 100
-puts b
+t = Enumerator.new{|y|
+  n = 0
+  loop {
+    m = (n+=1)
+    m /= 3 while m >= 3 && m % 3 != 2
+    y << n if m < 2
+  }
+}
+puts t.take(100)
