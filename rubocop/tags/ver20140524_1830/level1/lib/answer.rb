7c639eafc8a2e1eeb674cc275e7f0cb1070c840a
tomwot <tttomwottt@gmail.com>
2014-05-24 18:39:39 +0900

    18:30時点で最大警告のものをタグ付け。
    git-svn-id: file:///var/svn/repo/CodeIQ@86 a45ba2cc-39c1-924a-b1ab-54c9c331bf61

---
+++ rubocop/tags/ver20140524_1830/level1/lib/answer.rb
@@ -0,0 +1,8 @@
+class Answer 
+
+def a b=c#1
+ if
+(not@@d=""%e);f.
+g( ){||}while(!! h({:i=>[$j, ]*?k,},))end;end
+
+	end;
---
+++ rubocop/tags/ver20140524_1830/level2/lib/answer.rb
@@ -0,0 +1,6 @@
+class Answer 
+def a b=c
+if
+(not@@d=""%e);f.
+g(){||}while(!! h({ :i=>["#$j", ]*?k, },))end;end
+	end;
