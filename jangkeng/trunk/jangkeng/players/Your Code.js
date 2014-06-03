d72227a8fbbedd13d09a3398d33dd88e263e0b39
tomwot <tttomwottt@gmail.com>
2014-06-04 06:54:20 +0900

    まずはサンプル相手に全勝出来た手でコミット。
    git-svn-id: file:///var/svn/repo/CodeIQ@95 a45ba2cc-39c1-924a-b1ab-54c9c331bf61

--- jangkeng/trunk/jangkeng/players/Your Code.js
+++ jangkeng/trunk/jangkeng/players/Your Code.js
@@ -1,17 +1,81 @@
 "use strict";
 
 /*
-PlayerName="ここにプレイヤー名を書いて下さい。使える文字は（いわゆる半角の）英数字、空白、下線 のみです。" 
+PlayerName="tomwot" 
 【感想・作戦・工夫した点など】
-《この行を削除し、感想・作戦・工夫した点などを書いて下さい。複数行でも構いません》
+作戦：
+相手の出す手の周期が検出出来たらその周期に合わせて勝てる手を出す。
+それで負ける様だったら手を変える。
+感想：
+まずはお題相手に全勝出来た手で投稿してみます。
 */
+
+// 文字列の周期を検出する。
+function period(str) {
+    var n = str.length;
+
+    for( var s = 1 ; s < n ; ++s ) {
+        if ( str.slice(0, n - s) == str.slice(s, n) ) {
+            return s;
+        }
+    }
+    return n;
+
+}
+
+// main.js からのデッドコピーに近しいもの
+function score( hands, last ) {
+    var r = [0, 0];
+    var score_table = {
+        GC: 17,
+        CP: 10,
+        PG: 10 
+    };
+    for ( var i = hands[0].length - last ; i < hands[0].length ; ++i ) {
+        r[0] += score_table[ hands[0][i] + hands[1][i] ] || 0;
+        r[1] += score_table[ hands[1][i] + hands[0][i] ] || 0;
+    }
+    return r;
+}
+
+
+var hand_ini = "PGCG";
+var check_num = 8;
+
+var looped = false;
+var count = -1;
 
 function play(h)
 {
-  // この関数の実装を変更し、勝利を目指して下さい。
-  // ちなみにこのコードは、「直前に相手がパーを出していたらチョキを出す。それ以外はグーを出す」という意味です。
-  var last = h[1][h[1].length-1];
-  return last ==="P" ? "C" : "G";
+    count++;
+
+    // 敵の手のループを検出
+    var enemy_last_hand = h[1].slice(h[1].length - check_num, h[1].length);
+    var prod = period(enemy_last_hand);
+    if ( h[1].length * 2 > check_num && check_num > prod * 2 && !looped) {
+        looped = count;
+    }
+
+    if ( !looped ) {
+        return hand_ini[count%(hand_ini.length)];
+    }
+
+    var enemy_hand_estimate = "";
+    var s = score( h, 2 );
+
+    if ( count - looped > check_num && s[1] >= 20) {
+        // ループを検出して少し経つのに連敗しているときは手を変える。
+        enemy_hand_estimate = h[1][h[1].length - 1];
+    } else {
+        enemy_hand_estimate = enemy_last_hand[check_num % prod];
+    }
+    if ( enemy_hand_estimate == "P" ) {
+        return "C";
+    } else if ( enemy_hand_estimate == "G" ) {
+        return "P";
+    } else {
+        return "G";
+    }
 }
 
 // イベントを受け取り、グーチョキパーのどれを出すかを返信するための仕組みです。
