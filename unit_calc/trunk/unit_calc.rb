169982e06eb3c0cf6fd48924f6cc1903487cf0b9
tomwot <tttomwottt@gmail.com>
2014-09-04 07:07:44 +0900

    unit_calc.rb: リファクタリング。 共通の式を Equation#parse に入れた。 変数名修正。 単位換算式の持たせ方を変えた。でも気に入らないので次回再修正。
    git-svn-id: file:///var/svn/repo/CodeIQ@114 a45ba2cc-39c1-924a-b1ab-54c9c331bf61

--- unit_calc/trunk/unit_calc.rb
+++ unit_calc/trunk/unit_calc.rb
@@ -10,53 +10,41 @@ class Equation
   def solve
     left_str, right_str = @equ.split('=')
 
-    sign = '+'
-    left = parse(left_str).map do |un|
-      sign = un[0] = (un[0].empty? ? sign : un[0])
-      un
-    end
+    unit_nums_str = parse(left_str) + parse(right_str).map{|r| r[0] = (r[0] == '-' ? '+' : '-'); r}
 
-    sign = '+'
-    right = parse(right_str).map do |un|
-      sign = un[0] = (un[0].empty? ? sign : un[0])
-      un[0] = (un[0] == '-' ? '+' : '-')
-      un
-    end
+    unknown = unit_nums_str.find{|un| un[1] == ' '}
+    unit_nums_str.delete(unknown)
 
-    unitnums_str = left + right
-    unknown = unitnums_str.find{|un| un[1] == ' '}
-    unitnums_str.delete(unknown)
-
-    minimum_unit_nums = unitnums_str.map do |sign, num_str, unit|
+    minimum_unit_nums = unit_nums_str.map do |sign, num_str, unit|
       num = num_str.to_i
       num = -num if sign == unknown[0]
       num * minimum_unit_multiple(unit)
     end
 
-    @ans = minimum_unit_nums.reduce(&:+) / minimum_unit_multiple(unknown[2])
+    @ans = minimum_unit_nums.inject(&:+) / minimum_unit_multiple(unknown[2])
   end
 
   private
 
-  def parse(str)
-    str.scan(/([-+]?)(\d+| )([^-+\d ]+)/)
+  def parse(formula)
+    sign = '+'
+    formula.scan(/([-+]?)(\d+| )([^-+\d ]+)/).map do |unit_num_arr|
+      unit_num_arr[0] = sign if unit_num_arr[0].empty?
+      sign = unit_num_arr[0]
+      unit_num_arr
+    end
   end
 
   def minimum_unit_multiple(unit)
-    @hash ||= Hash[
-    *UNIT_NUMS_EQUATION.flat_map do |equation|
-      unit_nums = equation.split('=').map{|uns| parse(uns).flat_map{|un| [un[2], un[1].to_i]}}
-    unit_nums.flat_map{|uns| uns[1] = unit_nums.last[1] / uns[1]; uns}
+    @@hash ||= Hash[
+    *DATA.flat_map do |equation|
+      unit_nums = equation.chomp.split('=').map{|formula| parse(formula).flat_map{|un| [un[2], un[1].to_i]}}
+      unit_nums.flat_map{|uns| uns[1] = unit_nums.last[1] / uns[1]; uns}
     end
     ]
-    @hash[unit]
+    fail "Can't find unit(#{unit}) in unit table" unless @@hash.key?(unit)
+    @@hash[unit]
   end
-
-  UNIT_NUMS_EQUATION = [
-    '1km=1000m=100000cm=1000000mm',
-    '1kg=1000g=1000000mg',
-    '1“ú=24ŠÔ=1440•ª=86400•b'
-  ]
 end
 
 
@@ -73,3 +61,9 @@ ARGF.each do |line|
     puts equation.map{|e| "equation = #{e.equ}, answer = #{e.ans}"}
   end
 end
+
+
+__END__
+1km=1000m=100000cm=1000000mm
+1kg=1000g=1000000mg
+1“ú=24ŠÔ=1440•ª=86400•b
