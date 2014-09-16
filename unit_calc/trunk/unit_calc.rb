095244c0096c9f9b39036042def90dbab3fa8512
tomwot <tttomwottt@gmail.com>
2014-09-17 00:06:51 +0900

    クラスの構造変更。 一部の省略名称を正式名称に変更。
    git-svn-id: file:///var/svn/repo/CodeIQ@122 a45ba2cc-39c1-924a-b1ab-54c9c331bf61

--- unit_calc/trunk/unit_calc.rb
+++ unit_calc/trunk/unit_calc.rb
@@ -2,50 +2,54 @@
 
 class UnitsEquation
   class Formula
-    def self.parse(formula, unknown='')
+    def initialize(formula, unknown='')
+      @formula = formula
+      @unknown = unknown
+    end
+
+    def parse
       sign_before = '+'
-      formula.scan(/([-+]?)(\d+|#{unknown})([^-+\d#{unknown}]+)/).map do |sign, num_str, unit|
+      @formula.scan(/([-+]?)(\d+|#{@unknown})([^-+\d#{@unknown}]+)/).map do |sign, num_str, unit|
         sign = sign_before if sign.empty?
         sign_before = sign
         {sign: sign, num_str: num_str, unit: unit}
       end
     end
-
-    def self.unit_conversion_table
-      @unit_conversion_table ||= Hash[
-      *File.readlines(UNIT_CONVERSION).flat_map do |equation|
-        unit_nums = Hash[*equation.chomp.split('=').flat_map{|formula| parse(formula).flat_map{|un| [un[:unit], un[:num_str].to_i]}}]
-        unit_nums.flat_map{|unit, scale| [unit, unit_nums.values.max / scale]}
-      end
-      ]
-    end
-
-    UNIT_CONVERSION = 'unit_conversion.txt'
   end
 
-  attr_reader :equ, :ans
+  attr_reader :equation, :answer
 
-  def initialize(equ, unknown)
-    @equ = equ
-    @unknown = unknown
+  def initialize(equation, unknown)
+    @equation = equation
+    @unknown  = unknown
   end
 
   def solve
-    left_formula, right_formula = @equ.split('=')
+    left_formula, right_formula = @equ.split('=').map{|formula| Formula.new(formula, @unknown).parse}
+    unit_nums = left_formula + right_formula.map{|r| r[:sign] = (r[:sign] == '-' ? '+' : '-'); r}
 
-    unit_nums_formula = Formula.parse(left_formula, @unknown) + Formula.parse(right_formula, @unknown).map{|r| r[:sign] = (r[:sign] == '-' ? '+' : '-'); r}
+    unknown = unit_nums.find{|un| un[:num_str] == @unknown}
+    unit_nums.delete(unknown)
 
-    unknown = unit_nums_formula.find{|un| un[:num_str] == @unknown}
-    unit_nums_formula.delete(unknown)
-
-    minimum_unit_nums = unit_nums_formula.map do |unit_num|
+    minimum_unit_nums = unit_nums.map do |unit_num|
       num = unit_num[:num_str].to_i
       num = -num if unit_num[:sign] == unknown[:sign]
-      num * Formula.unit_conversion_table[unit_num[:unit]]
+      num * @@unit_conversion_table[unit_num[:unit]]
     end
 
-    @ans = minimum_unit_nums.inject(&:+) / Formula.unit_conversion_table[unknown[:unit]]
+    @answer = minimum_unit_nums.inject(&:+) / @@unit_conversion_table[unknown[:unit]]
   end
+
+  UNIT_CONVERSION = 'unit_conversion.txt'
+
+  @@unit_conversion_table =
+  File.readlines(UNIT_CONVERSION).each_with_object({}) do |equation, memo|
+    unit_nums = equation.chomp.split('=').each_with_object({}) do |formula, mm|
+      Formula.new(formula).parse.each{|f| mm[f[:unit]] = f[:num_str].to_i}
+    end
+    unit_nums.each{|unit, scale| memo[unit] = unit_nums.values.max / scale}
+  end
+
 end
 
 
@@ -55,11 +59,11 @@ if $0 == __FILE__
     no, *equ = line.chomp.split("\t")
     equation = equ.map{|e| UnitsEquation.new(e, 'Å†')}
     equation.each{|e| e.solve}
-    if equation.map(&:ans).uniq.size == 1
+    if equation.map(&:answer).uniq.size == 1
       puts "#{no}: Correct."
     else
       puts "#{no}: Wrong."
-      puts equation.map{|e| "  equation: #{e.equ}, answer: #{e.ans}"}
+      puts equation.map{|e| "  equation: #{e.equation}, answer: #{e.answer}"}
     end
   end
 end
