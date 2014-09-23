ad481b552fa43fd9c4ae59716631905358c9556c
tomwot <tttomwottt@gmail.com>
2014-09-24 01:35:25 +0900

    unit_conversion.txt を取り込んだ。
    git-svn-id: file:///var/svn/repo/CodeIQ@124 a45ba2cc-39c1-924a-b1ab-54c9c331bf61

--- unit_calc/trunk/unit_calc.rb
+++ unit_calc/trunk/unit_calc.rb
@@ -1,8 +1,6 @@
 #coding: Windows-31J
 
 # 単位付1元1次方程式を解く。
-# 準備:
-#   - 'unit_conversion.txt'を用意し、単位換算式を記述する。次元が同一であれば同一行に記載する。
 
 class UnitsEquation
   class Formula
@@ -32,7 +30,7 @@ class UnitsEquation
     left_formula, right_formula = @equation.split('=').map{|formula| Formula.new(formula, @unknown).parse}
     unit_nums = left_formula + right_formula.map{|r| r[:sign] = (r[:sign] == '-' ? '+' : '-'); r}
 
-    # 式の中で単位が換算出来ない場合の処理
+    # 方程式の中で単位が換算出来ない場合の処理
     # return unless unit_nums.uniq{|un| @@unit_conversion_table[un[:unit]][:base_unit]}.size == 1
 
     unknown = unit_nums.find{|un| un[:num_str] == @unknown}
@@ -47,10 +45,8 @@ class UnitsEquation
     @answer = minimum_unit_nums.inject(&:+) / @@unit_conversion_table[unknown[:unit]][:scale]
   end
 
-  UNIT_CONVERSION = 'unit_conversion.txt'
-
   @@unit_conversion_table =
-  File.readlines(UNIT_CONVERSION).map(&:chomp).each_with_object({}) do |equation, memo|
+  DATA.map(&:chomp).each_with_object({}) do |equation, memo|
     unit_nums = equation.split('=').each_with_object({}) do |formula, mm|
       Formula.new(formula).parse.each{|f| mm[f[:unit]] = f[:num_str].to_i}
     end
@@ -76,3 +72,8 @@ if $0 == __FILE__
     end
   end
 end
+
+__END__
+1km=1000m=100000cm=1000000mm
+1kg=1000g=1000000mg
+1日=24時間=1440分=86400秒
