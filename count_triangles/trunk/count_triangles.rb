0b4de0e9cfb138b2b90c066188a95201ff20756e
tomwot <tttomwottt@gmail.com>
2014-11-18 04:03:25 +0900

    初回提出分。
    git-svn-id: file:///var/svn/repo/CodeIQ@139 a45ba2cc-39c1-924a-b1ab-54c9c331bf61

---
+++ count_triangles/trunk/count_triangles.rb
@@ -0,0 +1,37 @@
+# CodeIQ：「中学入試から：図形と場合の数」
+# https://codeiq.jp/ace/nabetani_takenori/q1188
+
+# 円周上を等間隔で分割する点の集合から幾つかをピックアップし、
+# それらの点から何種類の三角形が作れるかを答える。
+# 回転して重なるものは同一、裏返して重なるものは別として考える。
+
+
+# #{n_pick}角形が何種類作れるかを返す。
+def solve(n_divide, points, n_pick)
+  answers = points.map{|p| ('A'..'Z').to_a.index(p)}.combination(n_pick).to_a.uniq do |po|
+    edges = po.push(po.first + n_divide).each_cons(2).map{|a, b| b - a}
+    n_pick.times.map{|n| edges.rotate(n)}.min
+  end
+  answers.count
+end
+
+
+$stdout.sync = true
+
+answer = []
+
+ARGF.each do |line|
+  id, n_divide, points, expect = line.chomp.split("\t")
+  estimate = solve(n_divide.to_i, points.split(','), 3)
+  if estimate == expect.to_i
+    puts "#{id}: Correct."
+  else
+    puts "#{id}: Wrong."
+    puts "  N: #{n_divide}, points: #{points}, expect: #{expect}, estimate: #{estimate}"
+    answer << id
+  end
+end
+puts
+puts "answer:"
+puts answer.join(',')
+
