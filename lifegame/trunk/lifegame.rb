cce1a6e3d98bec5110e9ea5dc8eaa03030543f31
tomwot <tttomwottt@gmail.com>
2015-11-07 08:13:00 +0900

    本日最終提出分。
    git-svn-id: file:///var/svn/repo/CodeIQ@196 a45ba2cc-39c1-924a-b1ab-54c9c331bf61

--- lifegame/trunk/lifegame.rb
+++ lifegame/trunk/lifegame.rb
@@ -2,5 +2,5 @@ n,h,w,*e=readlines
 N,H,W=[n,h,w].map(&:to_i)
 f=e.map{|h|h.chop.split('').map{|c|c==?*}}
 d=[*-1..1]
-N.times{f=f.map.with_index{|l,y|l.map.with_index{|m,x|(c=d.product(d).count{|w,z|(f+f)[w+y][z+x==W ? 0:z+x]})==3||m&&c==4}}}
+N.times{f=f.map.with_index{|l,y|l.map.with_index{|m,x|(c=d.product(d).count{|w,z|f[(w+y)%H][(z+x)%W]})==3||m&&c==4}}}
 puts f.map{|l|l.map{|c|c ? ?*:?.}*''}
