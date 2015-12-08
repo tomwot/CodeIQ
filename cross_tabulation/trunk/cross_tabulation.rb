00726f55c5bffe26fdddd062c1d19d09eb6e310d
tomwot <tttomwottt@gmail.com>
2015-12-08 23:32:07 +0900

    2回目提出分。 リファクタリング。
    git-svn-id: file:///var/svn/repo/CodeIQ@203 a45ba2cc-39c1-924a-b1ab-54c9c331bf61

--- cross_tabulation/trunk/cross_tabulation.rb
+++ cross_tabulation/trunk/cross_tabulation.rb
@@ -9,6 +9,12 @@ min_min   = -10
 min_max   =  40
 min_width =   5
 
+
+def headers(min, max, width, str)
+  (0..((max - min) / width)).map{|n| n * width + min}.each_cons(2).map{|a, b| "#{a}<=#{str}<#{b}"}
+end
+
+
 table = Array.new((max_max - max_min) / max_width){Array.new((min_max - min_min) / min_width, 0)}
 
 gets
@@ -18,7 +24,8 @@ ARGF.each do |line|
   table[(max - max_min) / max_width][(min - min_min) / min_width] += 1
 end
 
-column_headers = (0..((min_max - min_min) / min_width)).map{|n| n * min_width + min_min}.each_cons(2).map{|a, b| "#{a}<=min<#{b}"}
+column_headers = headers(min_min, min_max, min_width, 'min')
+row_headers    = headers(max_min, max_max, max_width, 'max')
 
 puts [nil, *column_headers].join(',')
-puts table.map.with_index{|row, i| ["#{i * max_width + max_min}<=max<#{(i + 1) * max_width + max_min}" ,*row].join(',')}
+puts row_headers.zip(table).map{|row| row.join(',')}
