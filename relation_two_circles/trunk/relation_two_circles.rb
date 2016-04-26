78de7fb56ab94c64e944e5ba2b07aa78113ab588
tomwot <tttomwottt@gmail.com>
2016-04-27 07:47:37 +0900

    提出分。
    git-svn-id: file:///var/svn/repo/CodeIQ@240 a45ba2cc-39c1-924a-b1ab-54c9c331bf61

--- relation_two_circles/trunk/relation_two_circles.rb
+++ relation_two_circles/trunk/relation_two_circles.rb
@@ -1,15 +1,33 @@
 # https://codeiq.jp/q/2785
 # 「２つの円の位置関係」
 
+X, Y, R = 0, 1, 2
+
 questions = ARGF.gets.chomp.split
 
-puts questions.map do |q|
+answers = questions.map do |q|
   circles_str = []
-  q.split('/').each_with_index{|c, i| _, *circles_str[i] = *c.match(/\((\d+),(\d+)\)(\d)/)}
-  circles = circle_str.map{|c| c.map(&:to_i)}
+  q.split('/').each_with_index{|c, i| _, *circles_str[i] = *c.match(/\((\d+),(\d+)\)(\d+)/)}
+  circles = circles_str.map{|c| c.map(&:to_i)}
 
-  case circles
-    when ->(circles){circles[0] == circles[1]}
-      puts 'A'
+  dist_center = (circles[0][X] - circles[1][X])**2 + (circles[0][Y] - circles[1][Y])**2
+  r = circles.map{|c| c[R]}
+
+  if dist_center == 0 && r[0] == r[1]
+    :A
+  elsif dist_center < (r[1] - r[0])**2
+    :B
+  elsif dist_center == (r[1] - r[0])**2
+    :C
+  elsif dist_center < (r[0] + r[1])**2
+    :D
+  elsif dist_center == (r[0] + r[1])**2
+    :E
+  elsif dist_center > (r[0] + r[1])**2
+    :F
+  else
+    raise 'Error'
   end
 end
+
+puts answers.join
