5bb829ef987aa7c339edb19f2efd55ff9893a39b
tomwot <tttomwottt@gmail.com>
2017-06-03 02:59:01 +0900

    最初のインポート
    git-svn-id: file:///var/svn/repo/CodeIQ@302 a45ba2cc-39c1-924a-b1ab-54c9c331bf61

---
+++ notHanoi/trunk/notHanoi.rb
@@ -0,0 +1,39 @@
+data = ARGF.eof? ? DATA : ARGF
+
+data.each do |line|
+	init = line.chomp.chars
+	towers_from = [[init, [], []]]
+	n = init.count
+	memo = {}
+	memo[towers_from.first.map(&:join).join(?,)] = true
+	iter = 0
+	catch(:found) do |found|
+		loop do
+			iter += 1
+			towers_to = []
+			towers_from.each do |towers|
+				towers.each_index do |i|
+					next if towers[i].empty?
+					towers.each_index do |j|
+						next if i == j
+						towers_next = towers.map{|t| t.dup}
+						head = towers_next[i].shift
+						towers_next[j].unshift(head)
+						next if towers_next[j].each_with_index.any?{|t, k| towers_next[j][0...k].count{|tt| tt > t} == 2}
+						throw found if j != 0 && towers_next[j].count == n
+						next if memo[towers_next.map(&:join).join(?,)]
+						towers_to << towers_next
+						memo[towers_next.map(&:join).join(?,)] = true
+					end
+				end
+			end
+			towers_from = towers_to
+		end
+	end
+	puts iter
+end
+
+__END__
+1234
+123
+1314
