f50a3cbcbb860b0a5b02107620ed88d8abce04e1
tomwot <tttomwottt@gmail.com>
2017-06-03 03:29:59 +0900

    初回提出分。
    git-svn-id: file:///var/svn/repo/CodeIQ@304 a45ba2cc-39c1-924a-b1ab-54c9c331bf61

--- notHanoi/trunk/notHanoi.rb
+++ notHanoi/trunk/notHanoi.rb
@@ -2,7 +2,7 @@ data = ARGF.eof? ? DATA : ARGF
 
 data.each do |line|
 	init = line.chomp.chars
-	towers_from = [[init, [], []]]
+	towers_from = [towers: [init, [], []], prev: nil]
 	n = init.count
 	memo = {}
 	memo[towers_from.first.map(&:join).join(?,)] = true
@@ -12,17 +12,17 @@ data.each do |line|
 			iter += 1
 			towers_to = []
 			towers_from.each do |towers|
-				towers.each_index do |i|
-					next if towers[i].empty?
-					towers.each_index do |j|
+				towers[:towers].each_index do |i|
+					next if towers[:prev] == i ||  towers[:towers][i].empty?
+					towers[:towers].each_index do |j|
 						next if i == j
-						towers_next = towers.map{|t| t.dup}
+						towers_next = towers[:towers].map{|t| t.dup}
 						head = towers_next[i].shift
 						towers_next[j].unshift(head)
 						next if towers_next[j].each_with_index.any?{|t, k| towers_next[j][0...k].count{|tt| tt > t} == 2}
 						throw found if j != 0 && towers_next[j].count == n
 						next if memo[towers_next.map(&:join).join(?,)]
-						towers_to << towers_next
+						towers_to << {towers: towers_next, prev: j}
 						memo[towers_next.map(&:join).join(?,)] = true
 					end
 				end
